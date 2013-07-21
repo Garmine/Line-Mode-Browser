eab4a1441f805c732b59d0c2f49ec82627f436c4
Garmine <mikro001@gmail.com>
2013-07-21 18:19:57 +0200

    Renamed the package "network" to "http" and renamed insides as well (since it is an HTTP client rather than some general network one)

--- net/garmine/network/ReqMethod.java
+++
@@ -1,5 +0,0 @@
-package net.garmine.network;
-
-public enum ReqMethod {
-	GET, POST, HEAD, OPTIONS, PUT, DELETE, TRACE;
-}
--- net/garmine/network/ResponseType.java
+++
@@ -1,5 +0,0 @@
-package net.garmine.network;
-
-public enum ResponseType {
-	RAW, SOURCE, /*AUTO, HTML, XML,*/ JSON, ERROR, NULL;
-}
